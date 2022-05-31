af9c3fc6e55179e5f95bd01a2f0e60b2520fc24e
MD. Shakhawat Hossain Shahin <Shakhawat.ch@gmail.com>
2022-05-31 16:21:36 +0600

    Initial Commit

---
+++ PracticalTest/PracticalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalTestApp.swift
+//  PracticalTest
+//
+//  Created by Shakhawat Hossain Shahin on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
